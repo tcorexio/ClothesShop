c8b66153936b2664c1832456e8947bed8cfa93df
thhn_trroq <133735405+Troqcoder-211@users.noreply.github.com>
2026-01-18 22:46:01 +0700

    handle exception & docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('test-exception/:id')
+  getException(@Param('id') id: string) {
+    if (id === '1') {
+      throw new BadRequestException('ID không hợp lệ');
+    }
+
+    if (id === '2') {
+      throw new NotFoundException('Không tìm thấy dữ liệu');
+    }
+
+    if (id === '3') {
+      throw new InternalServerErrorException('Lỗi hệ thống');
+    }
+
+    return {
+      message: 'OK',
+      id,
+    };
+  }
 
   @Get()
   getHello(): string {
