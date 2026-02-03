8583548c2c273f896d57f8a698c75cb1f37b0574
leetris2412 <trituanha@gmail.com>
2026-02-03 20:13:42 +0700

    wip(cart): update cart service interface

--- src/services/cart/cart.service.interface.ts
+++ src/services/cart/cart.service.interface.ts
@@ -3,7 +3,7 @@ import { UpdateCartItemDto } from '@dto/cart/update-cart-item.dto';
 import { CartModel } from '@models/cart/cart.model';
 import { CartItemModel } from '@models/cart/cart-item.model';
 
-export interface ICartService {
+export interface ICartService { 
   AddToCart(userId: number, dto: AddToCartDto): Promise<CartItemModel>;
   GetCart(userId: number): Promise<CartModel>;
   UpdateCartItem(
